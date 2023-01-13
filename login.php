4979fdb45ad22808311b21c05d07fe16abe0e58e
Himstedt <106549888+Himstedt@users.noreply.github.com>
2023-01-13 15:48:05 +0100

    Create login.php

---
+++ login.php
@@ -0,0 +1,41 @@
+<?php
+// Load php library
+require "membership.php";
+
+// check login credential
+if (count($_POST)!=0) {
+    $_MEM->verify($_POST["email"], $_POST["password"]);
+}
+
+// Redirect signed in users to membership page
+if (isset($_SESSIOn["member"])) {
+    header("Location: protect.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" >
+  <link type="text/css" rel="stylesheet" href="styles.css" />
+  <title>Login Page</title>
+ </head>
+
+ <body>
+     <!-- Login Form -->
+     <form method="post">
+         <h1>LOGIN</h1>
+
+         <label>Email</label>
+         <input type="email" name="email" required>
+
+         <label>Password</label>
+         <input type="password" name="password" required>
+
+         <input type="submit" value="Register">
+     </form>
+ </body>
+
+</html>
